32a43ca551f390ca394b4587b59616539a7bc814
Orjiewuru Kingdom Isaac <kingdomisaac@ymail.com>
2016-12-31 08:34:27 +0100

    First commit

---
+++ client/common.js
@@ -0,0 +1,22 @@
+Meteor.subscribe("posts");
+
+Meteor.subscribe("ProfileImages");
+
+Meteor.subscribe("UserImages");
+
+Meteor.startup(function() {
+	AccountsEntry.config({
+		homeRoute: '/',
+		dashboardRoute: '/',
+		passwordSignupFields: 'USERNAME_AND_EMAIL'
+	});
+
+	Accounts.ui.config({
+		passwordSignupFields: 'USERNAME_AND_EMAIL'
+	});
+});
+
+Template.registerHelper('getProfileImg', function(userId){
+	var imgUrl = UserImages.findOne({userId: userId}).image;
+	return imgUrl;
+});
